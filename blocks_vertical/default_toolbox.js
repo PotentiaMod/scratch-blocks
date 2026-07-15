ba805c7db4bb7562ebddd16ebd13dc34c953c1dd
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-07-15 07:05:24 -0400

    Update default_toolbox.js

--- blocks_vertical/default_toolbox.js
+++ blocks_vertical/default_toolbox.js
@@ -532,6 +532,7 @@ Blockly.Blocks.defaultToolbox = '<xml id="toolbox-categories" style="display: no
   '<category name="Extensions" id="extensions" colour="#FF6680" secondaryColour="#FF4D6A" ' +
     'iconURI="../media/extensions/wedo2-block-icon.svg" showStatusButton="true">' +
     '<block type="extension_pen_down" id="extension_pen_down"></block>' +
+	'options="extensionControls">' +
     '<block type="extension_music_drum" id="extension_music_drum">' +
       '<value name="NUMBER">' +
         '<shadow type="math_number">' +
