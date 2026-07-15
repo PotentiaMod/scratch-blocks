4dd325b11828edaedccc769a70e8b25993a70d1c
~ Gaia ~ <crystalfranks18@hotmail.com>
2026-07-15 04:04:38 -0400

    Increase toolbox width from 310 to 315

--- core/toolbox.js
+++ core/toolbox.js
@@ -112,7 +112,7 @@ Blockly.Toolbox.registerMenu = function(name, options, optionsMerge) {
  * This is the sum of the width of the flyout (250) and the category menu (60).
  * @type {number}
  */
-Blockly.Toolbox.prototype.width = 310;
+Blockly.Toolbox.prototype.width = 315;
 
 /**
  * Height of the toolbox, which changes only in horizontal layout.
